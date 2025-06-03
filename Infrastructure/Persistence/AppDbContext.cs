ee0eac1f52e852b76e97ff8d8433bdb15f9ac913
agustinleonardi <agustinleonardi5@gmail.com>
2025-06-03 15:23:20 -0300

    feat: configuración de AppDbContext y relaciones con EF Core

---
+++ Infrastructure/Persistence/AppDbContext.cs
@@ -0,0 +1,23 @@
+using Microsoft.EntityFrameworkCore;
+using UserManager.Domain.Entities;
+
+namespace UserManager.Infrastructure.Persistence;
+
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+    public DbSet<User> Users => Set<User>();
+    public DbSet<Role> Roles => Set<Role>();
+    public DbSet<Permission> Permissions => Set<Permission>();
+    public DbSet<UserRole> UserRoles => Set<UserRole>();
+    public DbSet<RolePermission> RolePermissions => Set<RolePermission>();
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<UserRole>().HasKey(ur => new { ur.UserId, ur.RoleId });
+        modelBuilder.Entity<RolePermission>().HasKey(rp => new { rp.RoleId, rp.PermissionId });
+
+        base.OnModelCreating(modelBuilder);
+    }
+}
